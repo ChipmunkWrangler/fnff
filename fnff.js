2a94deb7bccf2ce7991172e0243d325125b94f23
Rafael <>
2018-08-15 13:22:13 +0200

    Check radio button corresponding to hard-coded distance and listed range

--- fnff.js
+++ fnff.js
@@ -1,53 +1,50 @@
 "use strict";
-const POINT_BLANK = {
-    DC: 10,
-    listedRangeFraction: 0
-};
-const CLOSE = {
-    DC: 15,
-    listedRangeFraction: 0.25
-};
-
-const MEDIUM = {
-    DC: 20,
-    listedRangeFraction: 0.5
-};
-
-const LONG = {
-    DC: 25,
-    listedRangeFraction: 1
-};
-const EXTREME = {
-    DC: 30,
-    listedRangeFraction: 2
-};
-const TOO_FAR = {
-    DC: 999,
-    listedRangeFraction: 999
-};
 
 const RANGE_CATEGORIES = [
-    POINT_BLANK,
-    CLOSE,
-    MEDIUM,
-    LONG,
-    EXTREME,
-    TOO_FAR
+    {
+	id: "pointBlank",
+	DC: 10,
+	listedRangeFraction: 0
+    },
+    {
+	id: "close",
+	DC: 15,
+	listedRangeFraction: 0.25,
+    },
+    {
+	id: "medium",
+	DC: 20,
+	listedRangeFraction: 0.5,
+    },
+    {
+	id: "long",
+	DC: 25,
+	listedRangeFraction: 1
+    },
+    {
+	id: "extreme",
+	DC: 30,
+	listedRangeFraction: 2
+    },
+    {
+	id: "tooFar",
+	DC: 999,
+	listedRangeFraction: 999
+    },
 ]
 
-function GetRangeDC(dist, listedRange) {
+function GetRangeCategory(dist, listedRange) {
     for (let rangeCategory of RANGE_CATEGORIES) {
 	if (dist <= listedRange * rangeCategory.listedRangeFraction) {
-	    return rangeCategory.DC;
+	    return rangeCategory;
 	}
     }
 }
 
 
 const dist = document.getElementById("distance").value;
-console.log(dist);
 const listedRange = document.getElementById("listedRange").value;
-console.log( GetRangeDC(dist, listedRange) );
-
-const c = document.getElementById("rangeCategory");
-console.log(c);
+const rangeCategory = GetRangeCategory(dist, listedRange);
+const rangeCategoryElement = document.getElementById(rangeCategory.id);
+rangeCategoryElement.checked = true;
+console.log(rangeCategory.id);
